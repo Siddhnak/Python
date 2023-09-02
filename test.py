bacd75eb00ca4eda8a9a1041365ffb5d569be2c2
Siddhnak <siddhnak@gmail.com>
2023-09-02 13:32:25 +0530

    trying to build

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-print("This is easy..")
+# Guessing game using random until the ans is correct
 
-# hello there
+import random
+
+
+
+def Ggame():
+    rno=print(random.randint(1,14))
+    guess=0
+
+    
+    while guess!=rno:
+        # guess=int(input)
+        guess = int(input("enter your guess"))
+        print("That's not a correct option")
+
+    
+    print("you are correct")
+
+
+Ggame()
