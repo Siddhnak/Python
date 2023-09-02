785150a3b46de1b08993b33fefd12b8058ec1334
Siddhnak <siddhnak@gmail.com>
2023-09-02 11:29:44 +0530

    First commit

---
+++ Projects.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+# hello = "how are you"
+
+# print (f"HEy{hello}")
+
+# print("sir"+hello)
+
+"""         Find my LOVE💖 project      """
+
+#fstring is so nice here.. 
+
+# Lets frame it our way and try to add decision based on 
+# which we will give it a result of passed or fail
+
+print("Hi there\nFeel" 
+      +"awesome\nas you have been selected for the choice test " 
+      +"to marry our Sir.. ")
+
+print ("There goes your first question..")
+Regard= input("\nPlease type here\n")
+reg=Regard.upper()
+# STR="What do you think of our Sir as?"
+
+print(f" What do you think of our Sir as?{reg}")
+
+
+
+if "FRIEND" in reg:
+    print("Congrats.. you have answered Q1 correctly")
+else:
+    print("Naah..that's not correct")
+    
+
